4cbb620b2fe308de1d9e1c26a59df662f40d93f4
Tonycrz <tonybaidoo0@gmail.com>
2021-12-02 14:27:45 +0000

    list functions

---
+++ list_func.c
@@ -0,0 +1,40 @@
+#include "monty.h"
+
+/**
+ * delete_dnodeint_at_index - deltes a node in a doubly linked list
+ * at a given index
+ * @head: double pointer to the list
+ * @index: index of the node to delete
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int delete_dnodeint_at_index(stack_t **head, unsigned int index)
+{
+	stack_t *temp = *head;
+	unsigned int i = 0;
+
+	if (!index)
+	{
+		(*head) = temp->next;
+		if (temp->next)
+			temp->next->prev = NULL;
+		temp->next = NULL;
+		free(temp);
+		return (1);
+	}
+
+	while (i < index)
+	{
+		temp = temp->next;
+		i++;
+		if (!temp)
+			return (0);
+	}
+
+	temp->prev->next = temp->next;
+	if (temp->next)
+		temp->next->prev = temp->prev;
+	free(temp);
+
+	return (1);
+}
